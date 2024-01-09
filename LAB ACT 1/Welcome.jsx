e38bd08b94832d110313d3fadb54879adc6ca8e1
DhanaBermudez <98503778+DhanaBermudez@users.noreply.github.com>
2024-01-09 14:41:29 +0800

    Add files via upload

---
+++ LAB ACT 1/Welcome.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+export default class Welcome extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Hello World!</h1>
+               
+            </div>
+        );
+    }
+}
