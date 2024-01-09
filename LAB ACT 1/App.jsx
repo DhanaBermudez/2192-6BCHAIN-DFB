cec69b3d54d12232ed6fffb7e8e65bc4df8e74fb
DhanaBermudez <98503778+DhanaBermudez@users.noreply.github.com>
2024-01-09 14:50:50 +0800

    Add files via upload

---
+++ LAB ACT 1/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import Hello from "./components/hello";
+import Welcome from './components/welcome';
+import ParentComponent from './components/ParentComponent';
+
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <Hello/>
+       <Welcome/>
+       <h1> DHANA GRACEF. BERMUDEZ </h1>
+       <ParentComponent/>
+      </div>
+    
+    </>
+  )
+}
+
+export default App;
+//DHANA GRACE F. BERMUDEZ
+//WD - 401
