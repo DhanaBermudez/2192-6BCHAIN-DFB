c48bc0f5374c740420d7331abd52e7851297e0a7
DhanaBermudez <98503778+DhanaBermudez@users.noreply.github.com>
2024-01-09 14:50:12 +0800

    Add files via upload

---
+++ LAB ACT 1/components/ParentComponent.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ChildComponent from './ChildComponent';
+
+/*import ChildComponent from './ChildComponent';
+function ParentComponent() {
+    const dataToPass = "Hello from parent!";
+    return (
+        <ChildComponent message={dataToPass} />
+    );
+    }
+*/
+
+
+class ParentComponent extends React.Component{
+    render() {
+        const dataToPass = "Hello from parent!";
+        return (
+            <ChildComponent message={dataToPass} />
+        );
+    }
+}
+
+export default ParentComponent;
+
