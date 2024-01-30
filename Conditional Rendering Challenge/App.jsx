f26edbde96d448dc989aa1e397edb63a951bf5a6
DhanaBermudez <98503778+DhanaBermudez@users.noreply.github.com>
2024-01-30 14:46:51 +0800

    Add files via upload

---
+++ Conditional Rendering Challenge/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import './App.css'
+import Hello from "./components/hello";
+import Welcome from './components/welcome';
+import ParentComponent from './components/ParentComponent';
+import BookList from './components/BookList';
+import MovieList from './components/MovieList';
+import TodoList from './components/TodoList';
+import ConditionalDemo1 from './components/ConditionalDemo1';
+import ConditionalDemo2 from './components/Conditional-Demo2';
+import ConditionalDemo3 from './components/ConditionalDemo3';
+import ConditionalDemo4 from './components/ConditionalDemo4';
+import PrelimExam from './components/prelimexam';
+
+function App() {
+ 
+  return (
+    <>
+   
+    {/* <div>
+        <TodoList/>
+      </div>
+      <div>
+        <br></br>
+       <BookList/>
+       <br></br>
+      </div>
+      <br></br>
+      <br></br>
+      <div>
+       <MovieList/>
+      </div> 
+  <div>
+        <h2>This is a Conditional Demo 1</h2>
+        <ConditionalDemo1/>
+
+        <h2>This is a Conditional Demo 2</h2>
+        <ConditionalDemo2/>
+
+        <h2>This is a Conditional Demo 3</h2>
+        <ConditionalDemo3/>  
+
+        <h2>This is a Conditional Demo 4</h2>
+        <ConditionalDemo4/>  */}
+        <h1>Things to Bring</h1>
+  <PrelimExam/>
+   
+      
+
+
+    </>
+  )
+}
+
+export default App;
+//DHANA GRACE F. BERMUDEZ
+//WD - 401
