8cce3867368c2d86c389f8c8a4558356392acbc8
DhanaBermudez <98503778+DhanaBermudez@users.noreply.github.com>
2024-03-03 17:58:21 +0800

    Add files via upload

---
+++ Solidity Basics III/1_destructing.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract DestructuringDemo{
+    function multiOutput() public pure returns (uint, bool){
+        return (2, true);
+    }
+    function namedOutput() public pure returns (uint a, bool b){
+        return (1, true);
+    }
+    function varAssigned() public pure returns (uint a, bool b){
+        a = 1;
+        b = true;
+    }
+    function destructAssigned() public {
+        (uint a, bool b) = multiOutput();
+        (, b) = multiOutput();
+        //Log the values of a and b
+        emit LogValues(a, b);
+    }
+
+    event LogValues(uint a, bool _b);
+}
+
+//DHANA GRACE F. BERMUDEZ\
+//WD - 401
---
+++ Solidity Basics III/4_structs_demo.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+struct Todo{
+    string text;
+    bool completed;
+}
+
+//DHANA GRACE F. BERMUDEZ
+//WD - 401
