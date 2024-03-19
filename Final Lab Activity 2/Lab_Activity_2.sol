85a58b83fa7a7e57158c76adc1f217604329108b
DhanaBermudez <98503778+DhanaBermudez@users.noreply.github.com>
2024-03-19 15:03:46 +0800

    Add files via upload

---
+++ Final Lab Activity 2/Lab_Activity_2.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyFinalsActivity2 {
+address payable public owner;
+uint256 public constant INITIAL_ETH_AMOUNT = 2 ether;
+uint256 public remainingEth;
+uint256 public creationTime;
+
+event EtherReceived(address indexed from, uint256 amount);
+event EtherSent(address indexed to, uint256 amount);
+
+modifier onlyOwner() {
+require(msg.sender == owner, "Only the owner can call this function");
+_;
+}
+
+constructor() {
+owner = payable(msg.sender);
+creationTime = block.timestamp;
+remainingEth = INITIAL_ETH_AMOUNT;
+}
+
+receive() external payable {
+emit EtherReceived(msg.sender, msg.value);
+}
+function ReceivedAndEmit() external payable{
+    
+}
+
+
+function getBalance() public view returns (uint256) {
+return address(this).balance;
+}
+
+function sendEther(address payable _recipient, uint256 _amount) public onlyOwner {
+require(_amount <= remainingEth, "Insufficient funds in the contract");
+_recipient.transfer(_amount);
+remainingEth -= _amount;
+emit EtherSent(_recipient, _amount);
+}
+
+function destroy() public onlyOwner {
+selfdestruct(owner);
+}
+}
+
+//DHANA GRACE F. BERMUDEZ
+//In this activity, it shows how to send ether to thge other address and the other address should add 1. Also it shows the creation ti,e getbalance, initial eth, owner and remaining eth.
